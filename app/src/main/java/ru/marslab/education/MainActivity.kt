6ed79e8cb0ab61f4ba1309f23f7aa7d2f047832d
Sergey Aristov <aristov775@gmail.com>
2022-01-21 12:15:02 +0300

    add viewBinding to project

---
+++ app/src/main/java/ru/marslab/education/MainActivity.kt
@@ -0,0 +1,16 @@
+package ru.marslab.education
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.marslab.education.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
