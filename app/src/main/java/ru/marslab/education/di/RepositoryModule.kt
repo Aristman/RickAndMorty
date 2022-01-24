c03a113e737ddc7fef9e79ac031444064e86125c
Sergey Aristov <58388639+Aristman@users.noreply.github.com>
2022-01-24 12:28:26 +0300

    Revert "Create and release Characters Fragmnet with list of characters"

--- app/src/main/java/ru/marslab/education/di/RepositoryModule.kt
+++ app/src/main/java/ru/marslab/education/di/RepositoryModule.kt
@@ -1,18 +1,9 @@
 package ru.marslab.education.di
 
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import ru.marslab.education.data.CharacterRepositoryImpl
-import ru.marslab.education.data.retrofit.CharacterApi
-import ru.marslab.education.domain.repository.CharacterRepository
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RepositoryModule {
-
-    @Provides
-    fun provideCharacterRepository(characterApi: CharacterApi): CharacterRepository =
-        CharacterRepositoryImpl(characterApi)
-}
+object RepositoryModule
